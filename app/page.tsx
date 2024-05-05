c72658259b288119cd8fb99082d56c5b103c7603
olivetum <olivetum@mfleet.io>
2024-05-05 21:12:23 +0200

    Hero copy

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
           <div className="w-full max-w-[95ch] m-auto backdrop-blur-3xl text-white pt-12 pb-8 pl-12 pr-24 rounded-2xl bg-white/40">
               <h1 className="text-5xl mb-4 text-transparent font-semibold bg-gradient-to-br from-indigo-900 via-indigo-300 to-550% to-blue-800 bg-clip-text">Mood</h1>
-              <p className="mb-16 text-black/75">Track your mood though out your life. All you have to do is be honest.</p>
+              <p className="mb-16 text-black/75">Track your mood though out your life. Fully encrypted journal with history chart for your well being.</p>
               <div>
                   <Link href={href}>
                       <button className="bg-gradient-to-br from-violet-100 to-blue-200 py-2 px-4 rounded-lg font-semibold text-emerald-950">Get started</button>
